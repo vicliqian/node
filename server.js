10e9ad393d939402d609bddf224a145267a9c125
unknown <vicliqian@163.com>
2019-06-02 21:57:05 +0800

    'form-get-action'

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.get("/index",function(req,res){
    res.sendFile(__dirname+'/index.html')
 })
 
+app.get('/process_get',function(req,res){
+    var response = {
+        "first_name":req.query.first_name,
+        "last_name":req.query.last_name
+    }
+    res.send(JSON.stringify(response))
+})
+
 var server = app.listen(3000,function(){
    console.log(`run at localhost:3000`)
-   console.log(__dirname)
 })
