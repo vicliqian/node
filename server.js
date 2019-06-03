fd05c21190e66ecb343ffb3aab8c306e62c5203d
liqian <liqian@liqiandeMac-mini.local>
2019-06-03 10:56:07 +0800

    表单post

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var fs = require('fs');
 var app = express()
+var bodyParser = require('body-parser')
+/*
+* form 表单post
+* */
 // 静态文件
 app.use(express.static('../node', {
     index: false
@@ -10,11 +14,13 @@ app.get("/index",function(req,res){
    res.sendFile(__dirname+'/index.html')
 })
 
-app.get('/process_get',function(req,res){
+var urlencoded = bodyParser.urlencoded({extended:false})
+app.post('/process_get',urlencoded,function(req,res){
     var response = {
-        "first_name":req.query.first_name,
-        "last_name":req.query.last_name
+        "first_name":req.body.first_name,
+        "last_name":req.body.last_name
     }
+    console.log(response)
     res.send(JSON.stringify(response))
 })
 
