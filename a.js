74de11ee6973b2cb1f0e93a5813947c8329b1906
liqian <vicliqian@163.com>
2019-07-18 20:56:55 +0800

    '测试1'

---
+++ a.js
@@ -0,0 +1,20 @@
+测试后
+const sharedPropertyDefinition = {
+    enumerable:true,
+    configurable:true,
+    get: noop,
+    set: noop
+}
+function a(vm,sourceKey, key){
+    sharedPropertyDefinition.get = function(){
+        this[sourceKey][key]
+
+
+    }
+    sharedPropertyDefinition.set = function(val){
+       this[sourceKey][key] =val
+    }
+    Object.defineProperty(vm, key,sharedPropertyDefinition)
+
+
+}
